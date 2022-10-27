5d5fa43291bbe86acb9f4fed6a78d1f522c659d3
MiroslaVV <shlyify@gmail.com>
2022-10-28 00:26:44 +0200

    v2

---
+++ .history/demo-apka/src/app/post-list/post-list.component_20221028000402.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss'],
+})
+export class PostListComponent implements OnInit {
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.postsService.getAll().subscribe(console.log);
+  }
+}
--- .history/demo-apka/src/app/post.service_20221028000402.ts
+++ .history/demo-apka/src/app/post.service_20221028000402.ts
@@ -3,13 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from './post.model';
 
+
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class PostsService {
+
   constructor(private http: HttpClient) {}
 
   public getAll(): Observable<Post[]> {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
-  }
+    return  this.http.get('https://jsonplaceholder.typicode.com/posts');
+    }
+
 }
