f1e7d0c885da4b8adc4e0a21349f81545e8d0e55
MiroslaVV <shlyify@gmail.com>
2022-10-28 09:59:20 +0200

    bugfixes

--- demo-apka/src/app/app-routing.module.ts
+++ demo-apka/src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 const routes: Routes = [
+  { path: '', component: AppComponent },
+
   {
-  path: 'posts',
-  component: AppComponent
-  }
-  
+    path: 'posts',
+    component: PostListComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
